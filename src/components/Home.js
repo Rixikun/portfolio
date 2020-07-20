0396feaf06a4321ba3bad44b8f6079a47d9a4312
LinXia <linyianxia@hotmail.com>
2020-07-20 17:52:56 -0400

    add: cards w/ aos transition

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Navigation } from "./index";
 import { Parallax } from "react-parallax";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const bg_beach = require("../img/bg_beach.webp");
 const bg_beach_vertical = require("../img/bg_beach_vertical.jpg");
@@ -14,7 +16,7 @@ const Home = () => {
     <div className="home">
       <header>
         <Parallax
-          bgImage={bg_pastel_fractals}
+          bgImage={bg_cube}
           strength={800}
           bgImageStyle={{
             width: "100vw",
@@ -33,26 +35,71 @@ const Home = () => {
           <div style={{ height: "80vh" }}></div>
         </Parallax>
       </header>
-      <main>
-        <section>
-          <h3>About me</h3>
-          <div className="card">
-            <img src={egg} />
+      <main className="container">
+        <div className="verticalLine"></div>
+        <section className="card" data-aos="fade-in">
+          <div
+            className="img__container"
+            data-aos="fade-right"
+            data-aos-easing="ease-out-quart"
+            data-aos-duration="1200"
+            data-aos-delay="500"
+          >
+            <img src={cat} />
+          </div>
+          <div data-aos="fade-left" data-aos-easing="ease-out-quart">
+            <h3>About me</h3>
+            <p>
+              I'm Lin, a web developer that's just starting out and located in
+              New York, NY. I enjoy creating responsive layouts and intuitive
+              UI.
+            </p>
           </div>
         </section>
-        <section>
-          <h3> My work</h3>
-          <div className="card">
+        <section className="card" data-aos="fade-in">
+          <div
+            className="img__container"
+            data-aos="fade-left"
+            data-aos-easing="ease-out-quart"
+            data-aos-duration="1200"
+            data-aos-delay="500"
+          >
             <img src={egg} />
           </div>
+          <div data-aos="fade-right" data-aos-easing="ease-out-quart">
+            <h3> My work</h3>
+            <p>
+              Experienced in: JavaScript, Node, React, Redux, HTML, CSS, SCSS,
+              Express, Sequelize
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+            </p>
+          </div>
         </section>
-        <section>
-          <h3>Contact me</h3>
-          <div className="card">
-            <img src={egg} />
+        <section className="card" data-aos="fade-in">
+          <div
+            className="img__container"
+            data-aos="fade-right"
+            data-aos-easing="ease-out-quart"
+            data-aos-duration="1200"
+            data-aos-delay="500"
+          >
+            <img src={bg_beach_vertical} />
+          </div>
+          <div data-aos="fade-left" data-aos-easing="ease-out-quart">
+            <h3>Contact me</h3>
+            <p>email: linyianxia@gmail.com</p>
           </div>
         </section>
       </main>
+      <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+      {AOS.init({
+        offset: 400,
+        duration: 1000,
+      })}
     </div>
   );
 };
