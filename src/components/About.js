df0e4722ce687f89e59c62368f6552bf75e227e8
LinXia <linyianxia@hotmail.com>
2020-07-21 12:15:05 -0400

    color edit & hover/active state, clean up code

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,6 @@ import React from "react";
 const About = () => {
   return (
     <>
-      {" "}
       I'm Lin, a web developer that's just starting out and located in New York,
       NY. I enjoy creating responsive layouts and intuitive UI.
     </>
