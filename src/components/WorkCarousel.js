298bda1c6941a5534e925855f0e61dbfd5be82df
LinXia <linyianxia@hotmail.com>
2020-07-26 01:23:49 -0400

    fix: desktop-view carousel img alignment

--- src/components/WorkCarousel.js
+++ src/components/WorkCarousel.js
@@ -37,7 +37,7 @@ const WorkCarousel = () => {
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="d-block w-100 maxMobile"
             src={MobileJet}
             alt="Jetsweat Mobile"
           />
